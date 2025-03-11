1bd7332dc0a4f66cb92b2e6d2795ddffa6658a26
91939 <devguys500@gmail.com>
2025-03-11 17:47:09 +0530

    homepage starting

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get("/api/products", (req, res) => {
 
 // Serve index.html for all routes
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "home.html"));
 });
 
 // Start Server
